3916f7859601f84b29fd534c6232e532d85743b4
Kizinto <oscarwilliam1978@gmail.com>
2022-08-18 10:21:45 -0700

    first commit

---
+++ moonsun.js
@@ -0,0 +1,7 @@
+$('.tdnn').click(function () {
+    $("body").toggleClass('light');
+    $(".moon").toggleClass('sun');
+    $(".tdnn").toggleClass('day');
+  });
+  
+  <!--See version 2.0 withoug jQuery-->
